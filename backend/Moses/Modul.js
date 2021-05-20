be5dfb3bf8ece28f9c6c05af17fe95e50b33d653
DarkIceXD <15961071+DarkIceXD@users.noreply.github.com>
2021-05-20 17:35:49 +0200

    renamed get_first_group_of_regex to getFirstGroupOfRegex

--- backend/Moses/Modul.js
+++ backend/Moses/Modul.js
@@ -1,4 +1,4 @@
-function get_first_group_of_regex(regex, data) {
+function getFirstGroupOfRegex(regex, data) {
     const result = regex.exec(data)
     if (result && result.length > 1)
         return result[1]
@@ -21,43 +21,43 @@ class Modul {
     }
     static getTitel(html) {
         const regex = /<h1>([\s\S]*?)<\/h1>/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getLernergebnisse(html) {
         const regex = /Lernergebnisse<\/h3>\s*<\/div>\s*<div class="col-xs-12">\s*<span class="preformatedTextarea">([\s\S]*?)<\/div>/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getLehrinhalte(html) {
         const regex = /Lehrinhalte<\/h3>\s*<\/div>\s*<div class="col-xs-12">\s*<span class="preformatedTextarea">([\s\S]*?)<\/div>/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getFakultaet(html) {
         const regex = /Fakultät:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getSekretariat(html) {
         const regex = /Sekretariat:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getInstitut(html) {
         const regex = /Institut:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getFachgebiet(html) {
         const regex = /Fachgebiet:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getVerantwortlich(html) {
         const regex = /Verantwortliche Person:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getAnsprechpartner(html) {
         const regex = /Ansprechpartner:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
     static getEmail(html) {
         const regex = /E-Mail-Adresse:<\/label>[\s\S]*?\/>(.*)/
-        return get_first_group_of_regex(regex, html)
+        return getFirstGroupOfRegex(regex, html)
     }
 }
 
